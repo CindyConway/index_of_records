62a1159cb2daefd5b261d9ff751373853973f025
CindyConway <cindy@queen.att.net>
2014-12-13 09:40:36 -0800

    working on adding facets

--- app/routes/search_rt.js
+++ app/routes/search_rt.js
@@ -12,7 +12,7 @@ function getSearch(req, res){
   var query = '{"size":100, \
                 "query": { \
                   "multi_match": { \
-                        "query":"{search_terms}", \
+                        "query":"{{search_terms}}", \
                         "type": "most_fields", \
                         "fields": [ "title^10", \
                                     "department^9", \
@@ -29,6 +29,25 @@ function getSearch(req, res){
                                     "contact"], \
                         "tie_breaker":0.3, \
                         "minimum_should_match":"70%" \
+                    } \
+                  }, \
+                  "highlight": { \
+                    "pre_tags" : ["<strong class=\'search-highlight px1 regular \'>"], \
+                    "post_tags" : ["</strong>"], \
+                    "fields" : { \
+                      "title" : {}, \
+                      "department":{}, \
+                      "division": {}, \
+                      "category": {}, \
+                      "retention": {}, \
+                      "remarks": {}, \
+                      "on_site": {}, \
+                      "off_site": {}, \
+                      "total": {}, \
+                      "website":{}, \
+                      "contact":{}, \
+                      "link":{}, \
+                      "phone":{} \
                     } \
                   }, \
                   "aggs" : { \
@@ -57,13 +76,25 @@ function getSearch(req, res){
                   } \
                 }';
 
-  var query = query.replace('{search_terms}', body.terms);
+  var filter =  ',"post_filter": {   \
+                  "term" : {{search_filters}} \
+                }'
 
+  var query = query.replace('{{search_terms}}', body.criteria.terms);
+
+ //console.log(body.filters == '{}');
+  if(body.filters != '{}'){
+    query = query.substring(0, query.length - 1); //remove the last curly brace to insert filter
+
+    query = query + filter.replace('{{search_filters}}', body.filters) + '}';
+  }
+console.log(query);
   request.post({
     url:ES_SERVER + 'record_types/_search',
     form: query},
     function(err,httpResponse,body){
       if (err){res.send(err);}
+//console.log(body);
       res.send(body);
   })
 }
