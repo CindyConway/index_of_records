ad1c9d6306ac99e440c3dcfe56934c346b405b10
CindyConway <cindy@queen.local>
2014-12-19 15:12:20 -0800

    filtering logic complete

--- app/routes/search_rt.js
+++ app/routes/search_rt.js
@@ -9,122 +9,122 @@ function setup(app) {
 function getSearch(req, res){
   var body = req.body;
 
-  var search_fields = [ "title^10", "department^9", "division", "category^5", "retention"," remarks", "on_site", "off_site", "total", "website", "link", "phone", "contact"];
+  //create array of fields to search
+  var search_fields = [ "title^10"];
+  search_fields.push("department^9");
+  search_fields.push("division");
+  search_fields.push("category^5");
+  search_fields.push("retention");
+  search_fields.push("remarks");
+  search_fields.push("on_site");
+  search_fields.push("off_site");
+  search_fields.push("total");
+  search_fields.push("website");
+  search_fields.push("link");
+  search_fields.push("phone");
+  search_fields.push("contact");
+
+// Create the EleasticSearch query
+  var queryObj = {};
+  queryObj["size"] = 100;
+
+  queryObj["query"] = {};
+  queryObj.query["multi_match"] = {};
+  queryObj.query.multi_match["query"] = body.criteria.terms;
+  queryObj.query.multi_match["type"] = "most_fields";
+  queryObj.query.multi_match["fields"] = search_fields;
+  queryObj.query.multi_match["tie_breaker"] = 0.3;
+  queryObj.query.multi_match["minimum_should_match"] = "70%";
 
 
-  var query = {};
-  query["size"] = 100;
-  query["query"]= {};
-  query.query["filtered"] = {};
-  query.query.filtered["query"] = {};
-  query.query.filtered.query["multi_match"] = {};
-  query.query.filtered.query.multi_match["query"] = "transportation";
-  query.query.filtered.query.multi_match["type"] = "most_fields";
-  query.query.filtered.query.multi_match["fields"] = search_fields;
-  query.query.filtered.query.multi_match["tie_breaker"] = 0.3;
-  query.query.filtered.query.multi_match["minimum_should_match"] = "70%";
-  console.log(JSON.stringify(query, undefined, 2));
-  var query_string = JSON.stringify(query, undefined, 0);
+  //This is the query format needed to limit the filters as well as the search resutls
+  // I left this in here because I think the users may want this instead
+  // queryObj["query"]= {};
+  // queryObj.query.["query"] = {};
+  // queryObj.query.query["multi_match"] = {};
+  // queryObj.query.query.multi_match["query"] = body.criteria.terms;
+  // queryObj.query.query.multi_match["type"] = "most_fields";
+  // queryObj.query.query.multi_match["fields"] = search_fields;
+  // queryObj.query.query.multi_match["tie_breaker"] = 0.3;
+  // queryObj.query.query.multi_match["minimum_should_match"] = "70%";
+  // queryObj.query["filtered"] = {};
+  // queryObj.query.filtered["query"] = {};
+  // queryObj.query.filtered.query["multi_match"] = {};
+  // queryObj.query.filtered.query.multi_match["query"] = body.criteria.terms;
+  // queryObj.query.filtered.query.multi_match["type"] = "most_fields";
+  // queryObj.query.filtered.query.multi_match["fields"] = search_fields;
+  // queryObj.query.filtered.query.multi_match["tie_breaker"] = 0.3;
+  // queryObj.query.filtered.query.multi_match["minimum_should_match"] = "70%";
 
-//   var query = '{"size":100, \
-//                 "query": {\
-//                   "filtered" : {\
-//                     "query": { \
-//                       "multi_match": { \
-//                             "query":"{{search_terms}}", \
-//                             "type": "most_fields", \
-//                             "fields": [ "title^10", \
-                                        // "department^9", \
-                                        // "division", \
-                                        // "category^5", \
-                                        // "retention", \
-                                        // "remarks", \
-                                        // "on_site", \
-                                        // "off_site", \
-                                        // "total", \
-                                        // "website", \
-                                        // "link", \
-                                        // "phone", \
-                                        // "contact"], \
-//                             "tie_breaker":0.3, \
-//                             "minimum_should_match":"70%" \
-//                         } \
-//                       }, \
-//                       "highlight": { \
-//                         "pre_tags" : ["<strong class=\'search-highlight px1 regular \'>"], \
-//                         "post_tags" : ["</strong>"], \
-//                         "fields" : { \
-//                           "title" : {}, \
-//                           "department":{}, \
-//                           "division": {}, \
-//                           "category": {}, \
-//                           "retention": {}, \
-//                           "remarks": {}, \
-//                           "on_site": {}, \
-//                           "off_site": {}, \
-//                           "total": {}, \
-//                           "website":{}, \
-//                           "contact":{}, \
-//                           "link":{}, \
-//                           "phone":{} \
-//                         } \
-//                       }, \
-//                       "aggs" : { \
-//                       "departments" : { \
-//                           "terms" : { \
-//                             "field" : "department.og" \
-//                           }, \
-//                           "aggs" : { \
-//                           "division" : { \
-//                           "terms" : { \
-//                             "field" : "division.og" \
-//                           } \
-//                         } \
-//                       } \
-//                     }, \
-//                         "category" : { \
-//                           "terms" : { \
-//                             "field" : "category.og" \
-//                           } \
-//                         }, \
-//                         "retention" : { \
-//                           "terms" : { \
-//                             "field" : "retention.og" \
-//                           } \
-//                         } \
-//                       } \
-//                     }\
-//                 }\
-//               }';
-//
-//   var query = query.replace('{{search_terms}}', body.criteria.terms);
-// console.log(query);
-//
-//     if(Object.keys(body.filters).length != 0){
-//      var filter =  ',"post_filter": {   \
-//        "and" : [ \
-//        {{search_filters}} \
-//        ] \
-//      }';
-//
-//     var filters = body.filters;
-//     var filter_list = '';
-//
-//     for (var item in filters){
-//       filter_list += '{"terms": {"' + item + '": ["' + filters[item].join('","') + '"]}},';
-//     }
-//
-//     filter_list = filter_list.slice(0,-1); // remove the trailing comma
-//     query = query.substring(0, query.length - 1); //remove the last curly brace to insert filter
-//
-//     //insert the list of filters
-//     query = query + filter.replace('{{search_filters}}', filter_list) + '}'; // last curly brace added back in
-//
-//    }
+  // cause the search terms to return highlightes;
+  queryObj["highlight"] = {};
+  queryObj.highlight["pre_tags"] = ["<strong class=\'search-highlight px1 regular \'>"];
+  queryObj.highlight["post_tag"] = ["</strong>"];
+  queryObj.highlight["fields"] = {};
+  queryObj.highlight.fields["title"] = {};
+  queryObj.highlight.fields["department"] = {};
+  queryObj.highlight.fields["division"] = {};
+  queryObj.highlight.fields["category"] = {};
+  queryObj.highlight.fields["retention"] = {};
+  queryObj.highlight.fields["remarks"] = {};
+  queryObj.highlight.fields["on_site"] = {};
+  queryObj.highlight.fields["off_site"] = {};
+  queryObj.highlight.fields["total"] = {};
+  queryObj.highlight.fields["website"] = {};
+  queryObj.highlight.fields["contact"] = {};
+  queryObj.highlight.fields["link"] = {};
+  queryObj.highlight.fields["phone"] = {};
+
+  //cause aggregates to be returned. These are used as filters
+  queryObj["aggs"] = {};
+  queryObj.aggs["departments"] = {};
+  queryObj.aggs.departments["terms"] = {};
+  queryObj.aggs.departments.terms["field"] = "department.og";
+  queryObj.aggs.departments["aggs"] = {};
+  queryObj.aggs.departments.aggs["division"] = {};
+  queryObj.aggs.departments.aggs.division["terms"] = {};
+  queryObj.aggs.departments.aggs.division.terms["field"] = "division.og";
+  queryObj.aggs.category = {};
+  queryObj.aggs.category["terms"] = {};
+  queryObj.aggs.category.terms["field"] = "category.og";
+  queryObj.aggs.retention = {};
+  queryObj.aggs.retention["terms"] = {};
+  queryObj.aggs.retention.terms["field"] = "retention.og";
+
+
+  if(Object.keys(body.filters).length != 0){
+
+    // This filter will limit the search results, but the filters do not change
+    queryObj["post_filter"] = {};
+    queryObj.post_filter["and"] = [];
+
+    var filters = body.filters;
+    for (var item in filters){
+      obj = {};
+      obj.terms = {};
+      obj.terms[item] = filters[item];
+      queryObj.post_filter.and.push(obj);
+    }
+
+    // FILTER RESULTS **AND** FITLERS - User may ask me to do this instead
+    // queryObj.query.filtered["filter"] = {};
+    // queryObj.query.filtered.filter["bool"] = {};
+    // queryObj.query.filtered.filter.bool["must"] = [];
+    //
+    // var filters = body.filters;
+    // for (var item in filters){
+    //   obj = {};
+    //   obj.terms = {};
+    //   obj.terms[item] = filters[item];
+    //   queryObj.query.filtered.filter.bool.must.push(obj);
+    // }
+ }
+
+  var queryString = JSON.stringify(queryObj, undefined, 0);
 
   request.post({
     url:ES_SERVER + 'record_types/_search',
-    form: query_string},
+    form: queryString},
     function(err,httpResponse,body){
       if (err){res.send(err);}
       res.send(body);
