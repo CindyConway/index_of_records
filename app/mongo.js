b0e14851b389470533574e2a44547f85267acecc
Cindy Conway <cindyannconway@gmail.com>
2015-06-27 21:14:36 -0700

    added auth

--- app/mongo.js
+++ app/mongo.js
@@ -2,29 +2,29 @@ var mongodb = require('mongodb');
 
 module.exports.init = function(callback) {
 
-  var server = new mongodb.Server("127.0.0.1", 27017, {});
+  var server = new mongodb.Server("IP", PORT, {});
 
   new mongodb.Db('index_of_records', server, {
     w: 1
   })
   .open(function(error, client) {
 
-    module.exports.client = client;
+    client.authenticate('SOME_USERNAME', 'SOME_PASSWORD', function(err, result) {
+      module.exports.client = client;
 
-    module.exports.schedules = new mongodb.Collection(client, 'schedules');
-    module.exports.template = new mongodb.Collection(client, 'template');
-    module.exports.users = new mongodb.Collection(client, 'users');
+      module.exports.schedules = new mongodb.Collection(client, 'schedules');
+      module.exports.template = new mongodb.Collection(client, 'template');
+      module.exports.users = new mongodb.Collection(client, 'users');
 
-    module.exports.toObjectId = function(hexString) {
-      return mongodb.ObjectID.createFromHexString(hexString);
-    };
+      module.exports.toObjectId = function(hexString) {
+        return mongodb.ObjectID.createFromHexString(hexString);
+      };
 
-    module.exports.newObjectId = function() {
-      return mongodb.ObjectID();
-    };
+      module.exports.newObjectId = function() {
+        return mongodb.ObjectID();
+      };
+    });
 
-    //TO DO
-    //Need error handling
   });
 
 };
